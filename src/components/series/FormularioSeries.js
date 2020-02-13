5002a211d082022c8def54f06fdfc85ff7e70e46
WesleyMeneghini <wesley.meneghini@outlook.com.br>
2020-02-13 13:55:15 -0300

    Aplicando bootstrap para tabela

--- src/components/series/FormularioSeries.js
+++ src/components/series/FormularioSeries.js
@@ -29,49 +29,62 @@ class FormularioSeries extends Component {
     
     render() {
         return (
-            <div>
-                <form method="post" onSubmit={this.enviaDados}>
-                    <div className="form">
+            <div className="card">
+                <div className="card-header">
+                    Cadastro de Séries
+                </div>
+                <div className="card-body"> 
+                    <form method="post" onSubmit={this.enviaDados}>
+                        <div className="form-group">
 
-                        <label htmlFor="name">Nome</label>
-                        <input 
-                        type="text"
-                        id="nome"
-                        name="nome"
-                        value={this.state.nome}
-                        onChange={this.inputHandler}
-                        />
+                            <label htmlFor="name">Nome</label>
+                            <input 
+                                className="form-control mb-2"
+                                type="text"
+                                id="nome"
+                                name="nome"
+                                value={this.state.nome}
+                                onChange={this.inputHandler}
+                            />
 
-                        <label htmlFor="ano_lancamento">ano_lancamento</label>
-                        <input 
-                        type="number"
-                        id="ano_lancamento"
-                        name="ano_lancamento"
-                        value={this.state.ano_lancamento}
-                        onChange={this.inputHandler}
-                        />
+                            <label htmlFor="ano_lancamento">ano_lancamento</label>
+                            <input 
+                                className="form-control mb-2"
+                                type="number"
+                                id="ano_lancamento"
+                                name="ano_lancamento"
+                                value={this.state.ano_lancamento}
+                                onChange={this.inputHandler}
+                            />
 
-                        <label htmlFor="temporadas">temporada</label>
-                        <input 
-                        type="text"
-                        id="temporadas"
-                        name="temporadas"
-                        value={this.state.temporadas}
-                        onChange={this.inputHandler}
-                        />
+                            <label htmlFor="temporadas">temporada</label>
+                            <input 
+                                className="form-control  mb-2"
+                                type="text"
+                                id="temporadas"
+                                name="temporadas"
+                                value={this.state.temporadas}
+                                onChange={this.inputHandler}
+                            />
 
-                        <label htmlFor="sinopse">sinopse</label>
-                        <textarea 
-                        type="text"
-                        id="sinopse"
-                        name="sinopse"
-                        value={this.state.sinopse}
-                        onChange={this.inputHandler}
-                        ></textarea>
+                            <label htmlFor="sinopse">sinopse</label>
+                            <textarea
+                                className="form-control"
+                                type="text"
+                                id="sinopse"
+                                name="sinopse"
+                                value={this.state.sinopse}
+                                onChange={this.inputHandler}
+                            ></textarea>
 
-                        <button type="submit">Salvar</button>
-                    </div>
-                </form>
+                            <button
+                                className="btn btn-success form-control mt-2"
+                                type="submit">
+                                Salvar
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
--- src/components/series/TabelaSeries.js
+++ src/components/series/TabelaSeries.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const TabelaHead = () => {
 
     return(
-        <thead>
+        <thead className="thead-dark ">
             <tr>
                 <th>Nome</th>
                 <th>Lançamento</th>
@@ -36,11 +36,16 @@ class TabelaSeries extends Component {
     render() {
         const { series } = this.props
         return(
-            <div className="list">
-                <table>
-                    <TabelaHead />
-                    <TabelaBody  series={series}/>
-                </table>
+            <div className="card">
+                <div className="card-header">
+                    Lista de Séries
+                </div>
+                <div className="card-body">
+                    <table className="table table-striped table-hover">
+                        <TabelaHead />
+                        <TabelaBody  series={series}/>
+                    </table>
+                </div>
             </div>
         )
     }
